f7ec9e4270d48affcbeed755cdeec78b5d76f018
alias123 <alias123@alias123-N68S3B>
2017-03-08 12:24:17 +0530

    dddddddddddd

---
+++ src/test/java/guru99/gittest/AppTest.java
@@ -0,0 +1,27 @@
+package guru99.gittest;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class AppTest{
+	protected WebDriver driver;
+	
+	@Tes
+	
+	
+	 Public void gurututorials()
+	 {
+		WebDriver driver = new FireFoxDriver;
+		String etitile = "Meet guru";
+		String atitile = "";
+		
+		driver.get("");
+		
+		
+		atitle = driver.getTitle();
+		
+				
+	 }
+	
+}
